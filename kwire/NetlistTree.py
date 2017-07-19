0271d6c9f2eb5274d6c2e1217fed2e21c13e2776
kwan3217 <kwan3217@gmail.com>
2017-07-18 20:06:38 -0600

    Lots of work, 1bit adder now works

--- kwire/NetlistTree.py
+++ kwire/NetlistTree.py
@@ -1,7 +1,7 @@
 import antlr4
-from NetlistLexer   import NetlistLexer
-from NetlistParser  import NetlistParser
-from NetlistVisitor import NetlistVisitor
+from NetlistLexer   import NetlistLexer 
+from NetlistParser  import NetlistParser 
+from NetlistVisitor import NetlistVisitor 
  
 class NetlistTree(NetlistVisitor):
     @staticmethod
@@ -52,22 +52,32 @@ class NetlistTree(NetlistVisitor):
                 'nets'      :dicts["nets"      ]}
     def visitComponents(self,ctx:NetlistParser.StringContext):
         children=self.visitChildren(ctx)
-        return {"components":children[1:-1]}
+        components={}
+        for component in children[1:-1]:
+            ref=component["ref"]
+            del component["ref"]
+            components[ref]=component
+        return {"components":components}
     def visitComp(self,ctx:NetlistParser.StringContext):
         (dicts,_)=self.sep(self.visitChildren(ctx))
         return dicts
     def visitLibsource(self,ctx:NetlistParser.StringContext):
         (dicts,_)=self.sep(self.visitChildren(ctx))
-        return {"libsource":dicts}
+        return {"libsource":dicts,"part":dicts["lib"]+":"+dicts["part"]}
     def visitLibparts(self,ctx:NetlistParser.StringContext):
-        children=self.visitChildren(ctx)
-        return {"libparts":children[1:-1]}
+        (dicts,_)=self.sep(self.visitChildren(ctx))
+        return {"libparts":dicts}
     def visitLibpart(self,ctx:NetlistParser.StringContext):
         (dicts,_)=self.sep(self.visitChildren(ctx))
-        return dicts
+        return {dicts["lib"]+":"+dicts["part"]:dicts}
     def visitPins(self,ctx:NetlistParser.StringContext):
         children=self.visitChildren(ctx)
-        return {"pins":children[1:-1]}
+        pins={}
+        for pin in children[1:-1]:
+            num=pin["num"]
+            del pin["num"]
+            pins[num]=pin
+        return {"pins":pins}
     def visitPin(self,ctx:NetlistParser.StringContext):
         (dicts,_)=self.sep(self.visitChildren(ctx))
         return dicts
@@ -79,7 +89,12 @@ class NetlistTree(NetlistVisitor):
         return {dicts["name"]:nondicts[1]}
     def visitNets(self,ctx:NetlistParser.StringContext):
         children=self.visitChildren(ctx)
-        return {"nets":children[1:-1]}
+        nets={}
+        for net in children[1:-1]:
+            name=net["name"]
+            del net["name"]
+            nets[name]=net
+        return {"nets":nets}
     def visitNet(self,ctx:NetlistParser.StringContext):
         (dicts,nondicts)=self.sep(self.visitChildren(ctx))
         nodes=[]
@@ -121,18 +136,215 @@ class NetlistTree(NetlistVisitor):
     def visitRev    (self,ctx:NetlistParser.StringContext):return self.visitOptionalBoringNode(ctx)
     def visitTitle  (self,ctx:NetlistParser.StringContext):return self.visitOptionalBoringNode(ctx)
 
-def parseNetlist(infn):     
-    inf = antlr4.FileStream(infn)
-    lexer = NetlistLexer(inf)
-    stream = antlr4.CommonTokenStream(lexer)
-    parser = NetlistParser(stream)
-    tree = parser.netlist()
-    visitor=NetlistTree()
-    return visitor.visit(tree)
+class Component:
+    @staticmethod
+    def factory(ref,comp):
+        part=comp["part"]
+        if part=="kwire:AND"      : return KwireAND      (ref,comp)
+        if part=="kwire:OR"       : return KwireOR       (ref,comp)
+        if part=="kwire:NOT"      : return KwireNOT      (ref,comp)
+        if part=="kwire:XOR"      : return KwireXOR      (ref,comp)
+        if part=="kwire:CONTROL"  : return KwireCONTROL  (ref,comp)
+        if part=="kwire:INDICATOR": return KwireINDICATOR(ref,comp)
+    def __init__(self,ref,comp):
+        self.ref=ref
+        self.libsource=comp["libsource"]
+        self.part=comp["part"]
+        self.value=comp["value"]
+        self.outputs={}
+    def addOutput(self,name,net):
+        self.outputs[name]=net
+    def setInput(self,context,pin,value):
+        pass
+    def __str__(self):
+        return self.__dict__.__str__()
+    
+class KwireAND(Component):
+    def __init__(self,ref,comp):
+        self.A=False
+        self.B=False
+        super().__init__(ref,comp)
+    def setInput(self,context,pin,value):
+        if pin=='1':
+            if value==self.A: return
+            self.A=value
+        else:
+            if value==self.B: return
+            self.B=value
+        context.addEvent(context.time,self.outputs["3"],self.A and self.B)
 
+class KwireOR(Component):
+    def __init__(self,ref,comp):
+        self.A=False
+        self.B=False
+        super().__init__(ref,comp)
+    def setInput(self,context,pin,value):
+        if pin=='1':
+            if value==self.A: return
+            self.A=value
+        else:
+            if value==self.B: return
+            self.B=value
+        context.addEvent(context.time,self.outputs["3"],self.A or self.B)
+
+class KwireNOT(Component):
+    def __init__(self,ref,comp):
+        self.A=False
+        super().__init__(ref,comp)
+
+class KwireXOR(Component):
+    def __init__(self,ref,comp):
+        self.A=False
+        self.B=False
+        super().__init__(ref,comp)
+    def setInput(self,context,pin,value):
+        if pin=='1':
+            if value==self.A: return
+            self.A=value
+        else:
+            if value==self.B: return
+            self.B=value
+        context.addEvent(context.time,self.outputs["3"],self.A ^ self.B)
+
+class KwireCONTROL(Component):
+    def __init__(self,ref,comp):
+        self.Y=False
+        super().__init__(ref,comp)
+    def setInput(self,context,pin,value):
+        if value!=self.Y:
+            self.Y=value
+            context.addEvent(context.time,self.outputs["1"],value)
+
+class KwireINDICATOR(Component):
+    def __init__(self,ref,comp):
+        self.A=False
+        super().__init__(ref,comp)
+    def setInput(self,context,pin,value):
+        if value!=self.A:
+            self.A=value
+
+class Context:
+    def __init__(self,infn):
+        inf = antlr4.FileStream(infn)
+        lexer = NetlistLexer(inf)
+        stream = antlr4.CommonTokenStream(lexer)
+        parser = NetlistParser(stream)
+        tree = parser.netlist()
+        visitor=NetlistTree()
+        netlist=visitor.visit(tree)
+        self.time=0
+        self.eventList=[]
+        self.nets=netlist["nets"]
+        self.libparts=netlist["libparts"]
+        self.components={}
+        for ref,comp in netlist["components"].items():
+            self.components[ref]=Component.factory(ref,comp)
+        #Figure out the input for each net
+        for name,net in self.nets.items():
+            #For each node, figure out if the pin is an output
+            for node in net["nodes"][:]: #The [:] makes a quick copy of the list in order to iterate on it
+                comp=self.components[node["ref"]]
+                libpart=self.libparts[comp.part]
+                pin=libpart["pins"][node["pin"]]
+                if pin["type"]=="output":
+                    comp.addOutput(node["pin"],name)
+                    net["nodes"].remove(node)
+                    
+    def dispatchEvent(self):
+        #Find the event with the earliest time
+        if len(self.eventList)==0:
+            return False
+        event=self.eventList.pop(0)
+        self.time=event["time"]
+        net=event["net"]
+        for node in self.nets[net]["nodes"]: 
+            comp=self.components[node["ref"]]
+            comp.setInput(context,node["pin"],event["value"])
+        return True
+    def addEvent(self,time,net=None,value=False):
+        #TODO: Add a check such that if there is another event with the
+        #exact same name and time, then that event is replaced with this one.
+        self.eventList.append({"time":time,"net":net,"value":value})
+    
 if __name__ == '__main__':
-    netlist=parseNetlist('../../kicad/kwire/8bitadder.net')
-    for comp in netlist["components"]:
-        print(comp)
-    for net in netlist["nets"]:
-        print(net)
+    context=Context('../../kicad/kwire/1bitdriver.net')
+    print(context.time,context.components["SA101"].Y,
+                       context.components["SB101"].Y,
+                       context.components["SC101"].Y,
+                       context.components["DS101"].A,
+                       context.components["DK101"].A)
+    context.components["SA101"].setInput(context,"1",True)
+    while context.dispatchEvent():
+        for event in context.eventList:
+            #print(event)
+            pass
+    print(context.time,context.components["SA101"].Y,
+                       context.components["SB101"].Y,
+                       context.components["SC101"].Y,
+                       context.components["DS101"].A,
+                       context.components["DK101"].A)
+    context.components["SB101"].setInput(context,"1",True)
+    while context.dispatchEvent():
+        for event in context.eventList:
+            #print(event)
+            pass
+    print(context.time,context.components["SA101"].Y,
+                       context.components["SB101"].Y,
+                       context.components["SC101"].Y,
+                       context.components["DS101"].A,
+                       context.components["DK101"].A)
+    context.components["SA101"].setInput(context,"1",False)
+    while context.dispatchEvent():
+        for event in context.eventList:
+            #print(event)
+            pass
+    print(context.time,context.components["SA101"].Y,
+                       context.components["SB101"].Y,
+                       context.components["SC101"].Y,
+                       context.components["DS101"].A,
+                       context.components["DK101"].A)
+    context.components["SC101"].setInput(context,"1",True)
+    while context.dispatchEvent():
+        for event in context.eventList:
+            #print(event)
+            pass
+    print(context.time,context.components["SA101"].Y,
+                       context.components["SB101"].Y,
+                       context.components["SC101"].Y,
+                       context.components["DS101"].A,
+                       context.components["DK101"].A)
+    context.components["SA101"].setInput(context,"1",True)
+    while context.dispatchEvent():
+        for event in context.eventList:
+            #print(event)
+            pass
+    print(context.time,context.components["SA101"].Y,
+                       context.components["SB101"].Y,
+                       context.components["SC101"].Y,
+                       context.components["DS101"].A,
+                       context.components["DK101"].A)
+    context.components["SB101"].setInput(context,"1",False)
+    while context.dispatchEvent():
+        for event in context.eventList:
+            #print(event)
+            pass
+    print(context.time,context.components["SA101"].Y,
+                       context.components["SB101"].Y,
+                       context.components["SC101"].Y,
+                       context.components["DS101"].A,
+                       context.components["DK101"].A)
+    context.components["SA101"].setInput(context,"1",False)
+    while context.dispatchEvent():
+        for event in context.eventList:
+            #print(event)
+            pass
+    print(context.time,context.components["SA101"].Y,
+                       context.components["SB101"].Y,
+                       context.components["SC101"].Y,
+                       context.components["DS101"].A,
+                       context.components["DK101"].A)
+    context.components["SC101"].setInput(context,"1",False)
+    while context.dispatchEvent():
+        for event in context.eventList:
+            #print(event)
+            pass
